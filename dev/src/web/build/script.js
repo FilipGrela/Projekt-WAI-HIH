51cd6cd9b5ed4e14952de2c42d5efe81da36bd86
FilipGrela <fgrela09@gmail.com>
2024-10-10 23:49:48 +0200

    Add navbar

--- dev/src/web/build/script.js
+++ dev/src/web/build/script.js
@@ -1,5 +1,12 @@
-function displayTotalPerPerson(person, total) {
-    var message = "Total for " + person + " is " + total;
-    document.getElementById("totalMessage").innerText = message;
-}
+var navbar = document.querySelector('.navbar');
+var menus_btn = document.querySelector('.menu-btn');
+var close_btn = document.querySelector('.close-btn');
+menus_btn.addEventListener('click', function () {
+    navbar.classList.add('active2');
+    menus_btn.classList.add('hide_btn');
+});
+close_btn.addEventListener('click', function () {
+    navbar.classList.remove('active2');
+    menus_btn.classList.remove('hide_btn');
+});
 //# sourceMappingURL=script.js.map
--- dev/src/web/scripts/script.ts
+++ dev/src/web/scripts/script.ts
@@ -1,4 +1,13 @@
-function displayTotalPerPerson(person: string, total: number) {
-    let message: string = "Total for " + person + " is " + total;
-    document.getElementById("totalMessage").innerText = message;
-}
+let navbar = document.querySelector('.navbar');
+let menus_btn = document.querySelector('.menu-btn');
+let close_btn = document.querySelector('.close-btn');
+
+menus_btn.addEventListener('click', () => {
+    navbar.classList.add('active2');
+    menus_btn.classList.add('hide_btn');
+})
+
+close_btn.addEventListener('click', () => {
+    navbar.classList.remove('active2');
+    menus_btn.classList.remove('hide_btn');
+})
