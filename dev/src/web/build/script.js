2f5152582fcbf2841944e74f934832893601e90a
FilipGrela <fgrela09@gmail.com>
2024-10-15 22:57:36 +0200

    Fix JS error, Add navbar icon

--- dev/src/web/build/script.js
+++ dev/src/web/build/script.js
@@ -1,12 +1,18 @@
-var navbar = document.querySelector('.navbar');
-var menus_btn = document.querySelector('.menu-btn');
-var close_btn = document.querySelector('.close-btn');
-menus_btn.addEventListener('click', function () {
-    navbar.classList.add('active2');
-    menus_btn.classList.add('hide_btn');
-});
-close_btn.addEventListener('click', function () {
-    navbar.classList.remove('active2');
-    menus_btn.classList.remove('hide_btn');
-});
+function load_menu_buttons() {
+    var navbar = document.querySelector('.navbar');
+    var menus_btn = document.querySelector('.menu-btn');
+    var close_btn = document.querySelector('.close-btn');
+    menus_btn.addEventListener('click', function () {
+        navbar.classList.add('active2');
+        menus_btn.classList.add('hide_btn');
+    });
+    close_btn.addEventListener('click', function () {
+        navbar.classList.remove('active2');
+        menus_btn.classList.remove('hide_btn');
+    });
+    console.log("menu_buttons js loaded");
+}
+window.onload = function () {
+    load_menu_buttons();
+};
 //# sourceMappingURL=script.js.map
--- dev/src/web/scripts/script.ts
+++ dev/src/web/scripts/script.ts
@@ -1,13 +1,20 @@
-let navbar = document.querySelector('.navbar');
-let menus_btn = document.querySelector('.menu-btn');
-let close_btn = document.querySelector('.close-btn');
+function load_menu_buttons(): void {
+    let navbar = document.querySelector('.navbar');
+    let menus_btn = document.querySelector('.menu-btn');
+    let close_btn = document.querySelector('.close-btn');
 
-menus_btn.addEventListener('click', () => {
-    navbar.classList.add('active2');
-    menus_btn.classList.add('hide_btn');
-})
+    menus_btn.addEventListener('click', () => {
+        navbar.classList.add('active2');
+        menus_btn.classList.add('hide_btn');
+    })
 
-close_btn.addEventListener('click', () => {
-    navbar.classList.remove('active2');
-    menus_btn.classList.remove('hide_btn');
-})
+    close_btn.addEventListener('click', () => {
+        navbar.classList.remove('active2');
+        menus_btn.classList.remove('hide_btn');
+    })
+    console.log("menu_buttons js loaded");
+}
+
+window.onload = () => {
+    load_menu_buttons();
+}
