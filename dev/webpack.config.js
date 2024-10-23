afb69b1c5f6c7773550a74426ade73b5a9def9eb
FilipGrela <fgrela09@gmail.com>
2024-10-23 15:43:35 +0200

    Add tabele

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -1,39 +1,56 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-    entry: {
-        gallery: './src/web/scripts/gallery.ts',
-        script: './src/web/scripts/script.ts',
-        weather: './src/web/scripts/weather.ts',
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'src/web/build'),
-        chunkFilename: '[id].[chunkhash].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    devtool: 'source-map',  // Enable source maps
-    watch: true,
-    watchOptions: {
-        poll: 500, // Sprawdzaj zmiany co 1000 ms
-        ignored: /node_modules/, // Ignoruj zmiany w node_modules
-    },
-    cache: {
-        type: 'filesystem',  // Użyj systemu plików jako cache
-    },
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: {
+            gallery: './src/web/scripts/gallery.ts',
+            script: './src/web/scripts/script.ts',
+            weather: './src/web/scripts/weather.ts',
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'src/web/build'),
+            chunkFilename: '[id].[chunkhash].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        devtool: isProduction ? false : 'source-map',  // Enable source maps only in development
+        watchOptions: {
+            poll: 500,
+            ignored: /node_modules/,
+        },
+        cache: {
+            type: 'filesystem',
+        },
+        optimization: {
+            minimize: isProduction,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        compress: {
+                            drop_console: true,  // Remove console.* calls
+                            drop_debugger: true, // Remove debugger statements
+                        },
+                    },
+                }),
+            ],
+        },
+    };
 };
